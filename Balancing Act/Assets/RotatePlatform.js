77245558a173e81214f7b9d8e7739f60b53a7c59
malcmalc3 <malcolm.milton23@yahoo.com>
2018-03-11 16:28:05 -0400

    Control platform with mouse

---
+++ Balancing Act/Assets/RotatePlatform.js
@@ -0,0 +1,7 @@
+﻿#pragma strict
+
+var speed : float; //how fast the object should rotate
+ 
+  function Update(){
+      transform.Rotate(Vector3(-Input.GetAxis("Mouse Y"), 0.0, Input.GetAxis("Mouse X")) * Time.deltaTime * speed, Space.World);
+  }
